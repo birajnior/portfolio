fe50d99598d6624e1f07891343c39f1d4d3490f6
birajnior <ubiratanbernardo@gmail.com>
2022-10-13 11:46:22 -0300

    Melhorando a aparência do portfólio

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function changeTheme() {
-    document.body.classList.toggle("dark")
-}
+const input = document.querySelector("input");
+const body = document.querySelector("body");
 
+const toggleThemeMode = () => {
+  body.classList.toggle("dark");
+};
+
+input.onchange = toggleThemeMode;
+
+
+// function changeTheme() {
+//     document.body.classList.toggle("dark")
+// }
